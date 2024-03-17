563d6c160719e75f7661b2daf3995ac69a8e0e71
birb <parabirb@protonmail.ch>
2024-03-16 20:46:03 -0500

    push TTS module

---
+++ components/tts.js
@@ -0,0 +1,26 @@
+// deps
+import fs from "fs";
+import path from "path";
+import { cwd } from "process";
+import wavefile from "wavefile";
+import { pipeline } from "@xenova/transformers";
+import config from "../config.json" assert { type: "json" };
+
+// class
+export default class TTS {
+    // init function
+    async init() {
+        // make a pipeline
+        this.synthesizer = await pipeline("text-to-speech", config.tts, { quantized: false });
+    }
+    // speak function
+    async speak(text) {
+        // get output
+        let output = await this.synthesizer(text, { speaker_embeddings: config.ttsEmbeddings });
+        // turn it into a wav
+        const wav = new wavefile.WaveFile();
+        wav.fromScratch(1, output.sampling_rate, "32f", output.audio);
+        // write the wav
+        fs.writeFileSync(path.join(cwd(), "ame_speech.wav"), wav.toBuffer());
+    }
+}
--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 // deps
+import LLM from "./components/llm.js";
+import STT from "./components/stt.js";
+import TTS from "./components/tts.js";
 import config from "./config.json" assert { type: "json" };
-import { HumanMessage, SystemMessage } from "@langchain/core/messages";
-import { ChatLlamaCpp } from "@langchain/community/chat_models/llama_cpp";
-
-const llamaPath = "/home/parabirb/Tsuyu/models/gpt4all-falcon-newbpe-q4_0.gguf";
-
-const model = new ChatLlamaCpp({ modelPath: llamaPath });
-
-const response = await model.invoke([
-    new SystemMessage(config.prompt),
-    new HumanMessage("Generate a short poem for me.")
-]);
-console.log(`AI: ${response.content}`);
