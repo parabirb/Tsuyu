e995d5754d38b1039c525afa93c98b0fd26f12e6
birb <parabirb@protonmail.ch>
2024-03-24 22:52:47 -0500

    minor fixes

--- components/modules.js
+++ components/modules.js
@@ -12,7 +12,7 @@ function parseJson(input) {
 }
 
 // check if module exists
-function checkIfModuleExists(moduleName) {
+function checkIfModuleExists(moduleName, modules) {
     // loop through
     for (let module of modules) {
         if (module.title === moduleName) return module;
@@ -57,7 +57,6 @@ ${Object.keys(module.args)
         // prompt the llm
         let response = await this.llm
             .invoke(`Predict which module should be called based on the input and context. If no module should be called or if the module you want to call does not exist, return null. Also return null if you do not have all the arguments necessary to call the relevant module. Otherwise, return a JSON object with key "title" for the title and a key "args" with the relevant arguments.
-
 ${
     memory
         ? `These pieces of context may be relevant.
@@ -77,7 +76,7 @@ Do not output ANYTHING other than either the JSON or the word "null".`);
         // if it isn't json, return
         if (!response) return "N/A";
         // get the relevant module
-        let module = checkIfModuleExists(response.title);
+        let module = checkIfModuleExists(response.title, modules);
         // return if there is none
         if (!module) return "N/A";
         // return if args not met
